e1118f4dafddf3676b0938239de9023f104d7b1f
Eduardo <eduardo.peralta12395@gmail.com>
2025-01-19 21:38:14 -0500

    intento de enlistado

--- app Cha.js
+++ app Cha.js
@@ -1,19 +1,34 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
 // Aquí deberás desarrollar la lógica para resolver el problema.
 
+//VARIABLES
 let listaNombres = [];
-function lista (nombre) {
 
-}
-
+//FUNCION PARA AGREGAR AMIGOS.
 function agregarAmigo() {
     let nombre = document.getElementById("amigo").value;
-    listaNombres.push(nombre);
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.")
+    } else {
+        listaNombres.push(nombre);
+    }
     console.log(listaNombres);
     limpiarCaja()
 }
 
+//FUNCION PARA LIMPIAR LA CAJA DE ENTRADA.
 function limpiarCaja() {
     document.querySelector('#amigo').value = '';
 
+}
+
+//OBTENER EL ELEMENTO DE LA LISTA.
+let enlistar = document.getElementById("resultado");
+enlistar.innerHTML = "";
+
+for (let i of listaNombres) {
+    let li = document.createElement("li");
+    li.textContent = i;
+    enlistar.appendChild(li);
+    console.log(i)
 }
