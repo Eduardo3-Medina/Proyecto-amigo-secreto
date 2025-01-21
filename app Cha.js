37739b939f417b8e85a17c88dfc0dcc62d86ecd4
Eduardo <eduardo.peralta12395@gmail.com>
2025-01-21 13:05:01 -0500

    se enlisto los nombres

--- app Cha.js
+++ app Cha.js
@@ -2,18 +2,28 @@
 // Aquí deberás desarrollar la lógica para resolver el problema.
 
 let listaNombres = [];
-function lista (nombre) {
-
-}
 
 function agregarAmigo() {
     let nombre = document.getElementById("amigo").value;
-    listaNombres.push(nombre);
-    console.log(listaNombres);
-    limpiarCaja()
+    if (nombre === "") {
+        alert("Ingrese nombre por favor");
+    } else {
+        listaNombres.push(nombre);
+        actualizarLista();
+    }
+    limpiarCaja();
 }
 
 function limpiarCaja() {
     document.querySelector('#amigo').value = '';
+}
 
+function actualizarLista() {
+    let enlistado = document.querySelector(".result-list");
+    enlistado.innerHTML = ""; // Limpiar la lista antes de actualizar
+    listaNombres.forEach((nombre) => {
+        let li = document.createElement("li");
+        li.textContent = nombre;
+        enlistado.appendChild(li);
+    });
 }
